0d514c0ffbf92c3fe9865e1338578442fde6fe74
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-02 17:58:34 -0500

    delete and gets done

--- index.js
+++ index.js
@@ -23,21 +23,63 @@ server.get('/users', (req, res) => {
 			res.status(500).json({ errorMessage: 'The users information could not be retrieved.' });
 		});
 });
-
+// add a user
 server.post('/users', (req, res) => {
-	const userData = req.body;
+	const { name, bio } = req.body;
+	if (!name || !bio) {
+		res.status(400).json({ error: 'Please provide name and bio for the user.' });
+	} else {
+		db
+			.insert({ name, bio })
+			.then(({ id }) => {
+				db.findById(id).then((user) => {
+					res.status(201).json(user);
+				});
+			})
+			.catch((error) => {
+				console.log('error on the POST/users', error);
+				res.status(500).json({
+					errorMessage : 'There was an error while saving the user to the database',
+				});
+			});
+	}
+});
+//get ID directly when typed in
+server.get('/users/:id', (req, res) => {
+	const { id } = req.params;
 	db
-		.insert(userData)
+		.findById(id)
 		.then((user) => {
-			res.status(201).json(user);
+			if (user) {
+				res.status(200).json(user);
+			} else {
+				res.status(404).json({ error: 'The user with the specified ID does not exist.' });
+			}
 		})
 		.catch((error) => {
-			console.log('error on the POST/users', error);
-			res.status(500).json({
-				errorMessage : '"There was an error while saving the user to the database"',
-			});
+			console.log(error);
+			res.status(500).json({ error: 'The user information could not be retrieved.' });
 		});
 });
 
+//delete
+server.delete('/users/:id', (req, res) => {
+	const { id } = req.params;
+	db
+		.remove(id)
+		.then((deleted) => {
+			if (deleted) {
+				res.status(200).json({ message: 'hub removed successfuly' });
+			} else {
+				res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+			}
+		})
+		.catch((error) => {
+			console.log('error on the POST/hubs', error);
+			res.status(500).json({
+				errorMessage : 'The user could not be removed',
+			});
+		});
+});
 const port = 5000;
 server.listen(port, () => console.log(`\n API running on port ${port} \n`));
